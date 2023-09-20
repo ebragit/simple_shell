6743ac760ce36ce32524adecf088c7dd9d117f50
ebragit <outorinside@gmail.com>
2023-09-20 19:31:22 +0100

    fix some bugs

--- getenv.c
+++ getenv.c
@@ -1,34 +1,93 @@
 #include "shell.h"
-/**
- * _getenv - Get the content of a global variable
- * @global_var: Variable to extract from environ
- * Return: Pointer to the content of a variable, or NULL if fails
- */
-char *_getenv(char *global_var)
-{
-	int i = 0;
-	const char cutter[] = "=";
-	char *env_tok, *envdup, *env_tok_dup;
 
-	if (global_var != NULL)
+/**
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
 	{
-		if (environ == NULL)
-			return (NULL);
-		envdup = _strdup(environ[i]);
-		while (envdup != NULL)
-		{
-			env_tok = strtok(envdup, cutter);
-			if (_strcmp(env_tok, global_var) == 0)
-			{
-				env_tok = strtok(NULL, cutter);
-				env_tok_dup = _strdup(env_tok);
-				free(envdup);
-				return (env_tok_dup);
-			}
-			i++;
-			free(envdup);
-			envdup = _strdup(environ[i]);
-		}
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
 	}
-	return (NULL);
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
 }
